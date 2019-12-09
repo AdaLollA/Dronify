2b5492fed4b16d281ed6d09a9d626ffe01800eb6
unknown <unknown@example.com>
2019-12-09 15:05:14 +0100

    i18n init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    lang: any;
 
-  constructor() {}
+    constructor(public translate: TranslateService) {
+        this.translate.setDefaultLang('en');
+        // this.translate.use('de');
+    }
 
+    switchLanguage() {
+        this.translate.use(this.lang);
+    }
 }
