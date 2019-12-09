e98e6c6cbe21772becf309989ef294d880d24679
unknown <unknown@example.com>
2019-12-09 17:02:35 +0100

    init algorithms

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,6 +8,15 @@ import {TranslateService} from '@ngx-translate/core';
 })
 export class HomePage {
     lang: any;
+    unitFactor = 0.001;
+    weight: number;
+
+    energy: number;
+    maxHeight: number;
+    percentOfAllowedPowerAtMaxAlt: number;
+
+    govHeight = 30; // as regulated by the austrian government in most cities
+    govEnergy = 79; // as regulated by the austrian government in most cities
 
     constructor(public translate: TranslateService) {
         this.translate.setDefaultLang('en');
@@ -16,5 +25,42 @@ export class HomePage {
 
     switchLanguage() {
         this.translate.use(this.lang);
+    }
+
+    update() {
+        if (this.unitFactor && this.weight) {
+            this.energy = this.weight * this.unitFactor * this.govHeight * 9.81;
+
+            this.percentOfAllowedPowerAtMaxAlt = this.energy / (this.govEnergy / 100);
+
+            this.maxHeight = this.percentOfAllowedPowerAtMaxAlt > 100 ?
+                this.govHeight - this.govHeight * (this.percentOfAllowedPowerAtMaxAlt/100 - 1)
+                : this.govHeight;
+
+            console.log('---');
+            console.log('e: '+this.energy);
+            console.log('h: '+this.maxHeight); // todo if maxheight is negative the drone is illegal to be flown
+            console.log('p: '+this.percentOfAllowedPowerAtMaxAlt);
+        }
+    }
+
+    updateUnit($event: CustomEvent) {
+        this.unitFactor = $event.detail.value == 'g' ? 0.001 : 1;
+        this.update();
+    }
+
+    updateWeight($event: CustomEvent) {
+        this.weight = $event.detail.value;
+        this.update();
+    }
+
+    updateGovEnergy($event: CustomEvent) {
+        this.govEnergy = $event.detail.value;
+        this.update();
+    }
+
+    updateGovHeight($event: CustomEvent) {
+        this.govHeight = $event.detail.value;
+        this.update();
     }
 }
